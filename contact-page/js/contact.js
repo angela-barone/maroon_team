7263274e8aaf7d7d314727b764afd5e3b6bc0dd8
Ashie-03 <109604613+Ashie-03@users.noreply.github.com>
2023-02-07 15:05:57 +0530

    Update contact.js

--- contact-page/js/contact.js
+++ contact-page/js/contact.js
@@ -1,24 +1,24 @@
 // SEND BUTTON ANIMATION
 
-let sendBtn = document.querySelector(".send-btn");
+ let sendBtn = document.querySelector(".send-btn");
 
-sendBtn.addEventListener("click", () => {
-  let planePath = sendBtn.querySelector(".circle");
-  let planeIcon = sendBtn.querySelector(".plane-icon");
+      sendBtn.addEventListener("click", () => {
+        let planePath = sendBtn.querySelector(".circle");
+        let planeIcon = sendBtn.querySelector(".plane-icon");
 
-  planePath.classList.add("rotateCircle");
-  planeIcon.classList.remove("hide");
+        planePath.classList.add("rotateCircle");
+        planeIcon.classList.remove("hide");
 
-  setTimeout(() => {
-    planeIcon.classList.add("hide");
-    planePath.classList.remove("rotateCircle");
-  }, 2750);
+        setTimeout(() => {
+          planeIcon.classList.add("hide");
+          planePath.classList.remove("rotateCircle");
+        }, 2750);
 
-  setTimeout(() => {
-    sendBtn.classList.add("checked");
-    sendBtn.innerHTML = "Sent";
-  }, 3000);
-});
+        setTimeout(() => {
+          sendBtn.classList.add("checked");
+          sendBtn.innerHTML = "Sent";
+        }, 3000);
+      });
 
 // DARK MODE CODE
 
